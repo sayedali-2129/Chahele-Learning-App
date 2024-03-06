83ff185e963efb45470b1a672440ec371636e428
sayedali-2129 <sayedalipld@gmail.com>
2024-03-06 21:31:20 +0530

    exam terms and conditions

--- lib/view/exam_screen/screens/terms_and_conditions.dart
+++ lib/view/exam_screen/screens/terms_and_conditions.dart
@@ -1,6 +1,9 @@
 import 'package:chahele_project/utils/constant_colors/constant_colors.dart';
+import 'package:chahele_project/utils/constant_icons/constant_icons.dart';
+import 'package:chahele_project/utils/widgets/button_widget.dart';
 import 'package:chahele_project/view/exam_screen/widgets/appbar_terms.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 
 class ExamTandCScreen extends StatelessWidget {
@@ -30,6 +33,8 @@ class ExamTandCScreen extends StatelessWidget {
                   ]),
               child: Padding(
                 padding: const EdgeInsets.all(16),
+
+                //Exam Details
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -40,13 +45,170 @@ class ExamTandCScreen extends StatelessWidget {
                           fontWeight: FontWeight.w600,
                           color: ConstantColors.black.withOpacity(0.7)),
                     ),
+                    const Gap(16),
+                    const ExamDetailsRow(
+                        text: "30 Minutes", icon: ConstantIcons.timeIcon),
+                    const Gap(16),
+                    const ExamDetailsRow(
+                        text: "100 Marks", icon: ConstantIcons.markIcon),
+                    const Gap(16),
+                    const ExamDetailsRow(
+                        text: "10 Questions", icon: ConstantIcons.questionIcon),
                   ],
                 ),
               ),
             ),
           ),
+          const SliverGap(16),
+
+          //Exam Terms
+          SliverToBoxAdapter(
+            child: Container(
+              height: 300,
+              width: screenWidth,
+              decoration: BoxDecoration(
+                  color: ConstantColors.white,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 7,
+                        offset: const Offset(0, 1),
+                        color: ConstantColors.black.withOpacity(0.15))
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "CHAHEL Exam terms",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: ConstantColors.black.withOpacity(0.7)),
+                    ),
+                    const Gap(24),
+                    const Text(
+                      "Topics",
+                      style: TextStyle(
+                          color: ConstantColors.headingBlue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14),
+                    ),
+                    const Gap(8),
+                    Text(
+                      "Introduction to kalari  payattu, attack, deffence, poozhikadakam, olinj maattam",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: TextStyle(
+                          color: ConstantColors.black.withOpacity(0.5),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12),
+                    ),
+                    const Gap(8),
+                    const Text(
+                      "Terms",
+                      style: TextStyle(
+                          color: ConstantColors.headingBlue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14),
+                    ),
+                    const Gap(8),
+                    Text(
+                      "■  You have only one chance to assessment",
+                      style: TextStyle(
+                          color: ConstantColors.black.withOpacity(0.5),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    const Gap(8),
+                    Text(
+                      "■  30minuts overall questions",
+                      style: TextStyle(
+                          color: ConstantColors.black.withOpacity(0.5),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    const Gap(8),
+                    Text(
+                      "■  score in the top 50% to get certificate",
+                      style: TextStyle(
+                          color: ConstantColors.black.withOpacity(0.5),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    const Gap(8),
+                    Text(
+                      "■  10 questions",
+                      style: TextStyle(
+                          color: ConstantColors.black.withOpacity(0.5),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  ButtonWidget(
+                    buttonHeight: 42,
+                    buttonWidth: screenWidth,
+                    buttonColor: ConstantColors.mainBlueTheme,
+                    buttonText: "Agree & Continue",
+                    onPressed: () {},
+                  ),
+                  const Gap(8),
+                  OutlineButtonWidget(
+                    buttonHeight: 42,
+                    buttonWidth: screenWidth,
+                    buttonColor: const Color(0xffF5F5F5),
+                    buttonText: "Cancel",
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
+    );
+  }
+}
+
+//Exam Details
+class ExamDetailsRow extends StatelessWidget {
+  const ExamDetailsRow({
+    super.key,
+    required this.text,
+    required this.icon,
+  });
+  final String text;
+  final String icon;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SvgPicture.asset(
+          icon,
+          height: 14,
+          width: 14,
+        ),
+        const Gap(14),
+        Text(
+          text,
+          style: const TextStyle(
+              color: ConstantColors.headingBlue,
+              fontSize: 12,
+              fontWeight: FontWeight.w500),
+        )
+      ],
     );
   }
 }
